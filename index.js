a885a7f3d34b6d5d4f2f33b3c6598fef83ff6957
manavm1990 <manavm@visionwebsoft.com>
2019-05-01 21:20:39 -0500

    Add components into 'body' on page.

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ import Navigation from './components/Navigation';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
-console.log(Navigation, Content, Footer);
+// Use innerHTML property as a GETTER
+let initialHTML = document.body.innerHTML;
+
+// Use innerHTML property as a SETTER
+document.body.innerHTML = `
+  ${Navigation}
+  ${Content}
+  ${Footer}
+  ${initialHTML}
+  `;
