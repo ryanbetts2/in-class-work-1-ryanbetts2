282eb11768796068e9e1cdfc42e142a2f401ab52
manavm1990 <manavm@visionwebsoft.com>
2019-05-06 18:31:04 -0500

    Use #root for page.

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ import Navigation from './components/Navigation';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
-// Use innerHTML property as a GETTER
-let initialHTML = document.body.innerHTML;
-
 // Use innerHTML property as a SETTER
-document.body.innerHTML = `
+document.querySelector('#root').innerHTML = `
   ${Navigation}
   ${Content}
   ${Footer}
-  ${initialHTML}
   `;
