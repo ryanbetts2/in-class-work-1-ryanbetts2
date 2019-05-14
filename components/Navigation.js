23e0799d42c157837926a660c2028532d29939d1
manavm1990 <manavm@visionwebsoft.com>
2019-05-14 17:53:35 -0500

    Pass 'links' into nagivation.

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,15 +1,14 @@
-export default () => `<nav>
+export default (state) => `<nav>
       <ul class="flex">
-        <li><a href="about.html">About Me</a></li>
-        <li><a href="contact.html">Contact</a></li>
+        <li><a href="about.html">${state.links[0]}</a></li>
+        <li><a href="contact.html">${state.links[1]}</a></li>
         <li>
           Portfolio
           <ul id="dropdown">
-            <li><a href="class.html">Class Showcase</a></li>
-            <li><a href="web-store.html">Web Store Hack-a-Thon</a></li>
-            <li><a href="demo.html">Demo Day Project</a></li>
+            <li><a href="class.html">${state.links[2]}</a></li>
+            <li><a href="web-store.html">${state.links[3]}</a></li>
+            <li><a href="demo.html">${state.links[4]}</a></li>
           </ul>
         </li>
       </ul>
     </nav>`;
-
