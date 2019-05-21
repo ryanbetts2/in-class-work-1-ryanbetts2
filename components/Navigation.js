d6dbbc8824e29f31d0f40852772a6cc094ff615f
manavm1990 <manavm@visionwebsoft.com>
2019-05-20 19:53:20 -0500

    Add icons.

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,8 +1,18 @@
+function buildIconHTML(link){
+    // If link.icon actually exists on this link, it is 'truth-y' so the `if` will take effect.
+    // If link.icon is not there for this link - it's UNDEFINED, of 'false-y' so the 'if' will NOT take effect.
+    if(link.icon){
+        return `<span class="${link.icon}"></span>`;
+    }
+
+    return '';
+}
+
 function buildNavHTML(stateLinks){
     let linksHTML = '';
 
     stateLinks.forEach((link) => {
-        linksHTML += `<li><a href="">${link.text}</a></li>`;
+        linksHTML += `<li><a href="">${buildIconHTML(link)}${link.text}</a></li>`;
     });
 
     return linksHTML;
