ae2b64d169d89b73610be95d607a38021f8da17b
manavm1990 <manavm@visionwebsoft.com>
2019-05-20 19:53:55 -0500

    Use closest() to resolve :bug: and make 'textContent' comes from an <a>.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function render(state){
     links.forEach((link) => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            render(states[`${e.target.textContent}`]);
+            render(states[`${e.target.closest('nav a').textContent}`]);
         });
     });
 }
