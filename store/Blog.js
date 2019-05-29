cb00996a77874f68840ecedf9f6bd508d0565a1c
manavm1990 <manavm@visionwebsoft.com>
2019-05-29 18:59:04 -0500

    Add Blog store data.

---
+++ store/Blog.js
@@ -0,0 +1,9 @@
+import links from './Links';
+
+export default {
+    // Value of pageContent corresponds with one of the modules containted in Pages
+    'pageContent': 'Blog',
+    'title': 'The Blog',
+    'links': links,
+    'posts': []
+};
