4a172a7602152621e3d8482cc8f1e38a8cac347d
manavm1990 <manavm@visionwebsoft.com>
2019-05-29 19:00:58 -0500

    Add Blog text to the menu.

--- store/Links.js
+++ store/Links.js
@@ -1,7 +1,7 @@
 export default
 {
     'primary': [
-        { 'text': 'Home', 'icon': 'fas fa-home' }, { 'text': 'About', 'icon': 'fas fa-id-card-alt' }, { 'text': 'Contact', 'icon': 'fas fa-phone' } ],
+        { 'text': 'Home', 'icon': 'fas fa-home' }, { 'text': 'About', 'icon': 'fas fa-id-card-alt' }, { 'text': 'Contact', 'icon': 'fas fa-phone' }, { 'text': 'Blog' } ],
     'dropdown': [ { 'text': 'Class Project' } , { 'text': 'Hack-a-Thon' }, { 'text': 'Demo Day' } ]
 };
 
