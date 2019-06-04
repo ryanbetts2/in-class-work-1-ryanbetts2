86f3b5bf935e5aa81c43e409937eab9c4d4ff489
manavm1990 <manavm@visionwebsoft.com>
2019-06-03 19:53:26 -0500

    Update Blog page with live posts data.

--- components/pages/Blog.js
+++ components/pages/Blog.js
@@ -1,3 +1,14 @@
+function buildPostsHTML(posts){
+    return posts.map((post) => `<article style="background: rgba(255, 255, 255, 0.8);">
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+    </article>`
+    );
+}
+
 export default function(state){
-    return `<h1>Hello from the Blog! ${state.title}</h1>`;
+    return `
+    <h1>${state.title}</h1>
+    ${buildPostsHTML(state.posts)};
+    `;
 }
