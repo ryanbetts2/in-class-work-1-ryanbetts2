c7448fcbd1aa9c1fdf0fb91bc847c26a6e3d9f6b
manavm1990 <manavm@visionwebsoft.com>
2019-07-09 21:09:55 -0500

    Replace h1's text content with greeting with name.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 // Create a greeter fxn. that will use the person's name and display, "Hello, John!" in <h1>.
 
-/*
-  3. Construct or concatenate a string with 'hello' and the value stored in memory for their name.
-  4. Select the <h1> on the page.
-  5. Replace the value of the <h1> tag.
-*/
 const name = prompt('What is ur name?');
+
+// const h1 = document.querySelector('h1');
+
+// h1.textContent = `Hello, ${name}`;
+
+document.querySelector('h1').textContent = `Hello, ${name}`;
