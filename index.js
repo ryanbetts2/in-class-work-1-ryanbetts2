dad43429dd67721492b0e6edf99f2a0a1ed0eaaf
manavm1990 <manavm@visionwebsoft.com>
2019-07-15 21:11:55 -0500

    Use parcel to render home page as SPA. 👏🏾

---
+++ index.js
@@ -0,0 +1,11 @@
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+document.querySelector('#root').innerHTML = `
+  ${Navigation}
+  ${Header}
+  ${Main}
+  ${Footer}
+`;
--- scripts.js
+++
@@ -1,9 +0,0 @@
-let fname = prompt('What is ur first name?');
-let lname = prompt('What is ur last name?');
-
-while(!fname || !lname){
-    fname = prompt('What is ur first name?');
-    lname = prompt('What is ur last name?');
-}
-
-document.querySelector('#greeting').innerHTML = `<p>Hello, ${fname} ${lname}</p>`;
