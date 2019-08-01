c55a510a9833d98085c5a5b4b050b20fb1728a5a
manavm1990 <manavm@visionwebsoft.com>
2019-07-31 20:32:22 -0500

    Usse page from state.

--- components/Main.js
+++ components/Main.js
@@ -1,42 +1,7 @@
 export default function(state){
     return `
     <main>
-      <!-- Sections are like subtopics directly related to the general topic of the page. -->
-      <section>
-        <h2>This Is My Top Section</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis nulla
-          temporibus enim, qui quo sed quisquam itaque dolorum laborum nam
-          necessitatibus corrupti ratione porro vitae perspiciatis! Pariatur
-          reprehenderit aut sed!
-        </p>
-
-        <a href="#" class="cta-btn">Read More!</a>
-      </section>
-
-      <section>
-        <h2>This Is My Top Section</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis nulla
-          temporibus enim, qui quo sed quisquam itaque dolorum laborum nam
-          necessitatibus corrupti ratione porro vitae perspiciatis! Pariatur
-          reprehenderit aut sed!
-        </p>
-
-        <a href="#" class="cta-btn">Read More!</a>
-      </section>
-
-      <section>
-        <h2>This Is My Top Section</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis nulla
-          temporibus enim, qui quo sed quisquam itaque dolorum laborum nam
-          necessitatibus corrupti ratione porro vitae perspiciatis! Pariatur
-          reprehenderit aut sed!
-        </p>
-
-        <a href="#" class="cta-btn">Read More!</a>
-      </section>
+      ${state.page}
     </main>
 `;
 }
