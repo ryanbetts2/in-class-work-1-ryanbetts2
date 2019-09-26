9cce794347e6c44b3c1ab821a870caedc132c30d
Ryan Betts <ryan.betts@student.swic.edu>
2019-09-25 19:26:52 -0500

    Use while loop to sum numbers from 1 to 10 and modify index.html

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// TODO: Sum the numbers from 1 to 10
+console.log(1+2+3+4+5);
+
+// Keep track of starting and ending - Iterator
+// Tracking the current sum or total
+
+let i = 1;
+let sum = 0;
+
+
+while (i <= 10 ) {
+  sum += i;
+  i += 1; // i = i + 1;
+}
+
+console.log(sum);
