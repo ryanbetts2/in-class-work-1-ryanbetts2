4d0a8c5e86a0c6cc92a9a72dd0614e73d3cd17c5
Ryan Betts <ryan.betts@student.swic.edu>
2019-10-02 20:48:30 -0500

    catch up on code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
-const burger = document.querySelector('.fa-hamburger')
+const burger = document.querySelector('.fa-hamburger');
+const ul =document.querySelector('nav ul');
 
+burger.addEventListener('click', {} => {
+  ul.classList.toggle('is-hidden--mobile')
+});
