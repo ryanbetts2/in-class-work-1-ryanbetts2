3e40fa7ca63f055dc0063d02c87d4f38f1621967
Ryan Betts <ryan.betts@student.swic.edu>
2019-10-02 19:53:45 -0500

    add querySelector for burger

--- scripts.js
+++ scripts.js
@@ -1,21 +1,2 @@
-// TODO: Sum the numbers from 1 to 10
-// keyword - expression in parentheses - {}
-// Function Expression - we specify name parameters which are assigned values based on arguments that we pass upon invocation
-function numberSummer(start, end) {
-  let sum = 0;
-  for(let i = start; i <= end; i += 1) {
-  sum += i;
-}
-console.log(sum);
+const burger = document.querySelector('.fa-hamburger')
 
-return sum;
-}
-
-console.log(numberSummer(1,10));
-
-
-// for(let i = 1; i <= 10; i += 1) {
-// sum += i;
-// }
-
-// console.log(sum);
