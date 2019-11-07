6644f2cc661994bf6bd2331ea95966ea50e9df39
Ryan Betts <ryan.betts@student.swic.edu>
2019-11-06 19:13:53 -0600

    Destructuring

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.fa-hamburger');
-const ul =document.querySelector('nav ul');
-
-burger.addEventListener('click', {} => {
-  ul.classList.toggle('is-hidden--mobile')
-});
